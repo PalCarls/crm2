d4d2bd36522d8d623708e81ce1ab2dae7489a80e
Luis Ricardo Nardelli <86408252+magosdigital@users.noreply.github.com>
2023-08-17 16:52:47 -0300

    Initial

--- frontend/src/utils/formatToCurrency.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable new-cap */
-export default function formatToCurrency(value) {
-  return Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(value);
-}
